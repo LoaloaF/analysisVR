1cfd02f801916c437c289a9dd800932e2735df84
vrmaster <vrmaster@itet-staff-ntg-als-spec-2-170.ethz.ch>
2024-12-18 17:54:06 +0100

    event first look

--- ephys_event.py
+++ ephys_event.py
@@ -6,6 +6,7 @@ import time
 import scipy.io
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+import mat73
 
 def pca_on_clusters(clusters):
     pca = PCA(n_components=2)  # Reduce to 2 principal components for visualization
@@ -26,8 +27,11 @@ def pca_on_clusters(clusters):
     return np.argsort(-np.abs(pca.components_[0]))
 
 def plot_event_ephys(event_type, behavior_event, clusters, time_window):
-    df_event = behavior_event[df_event["event_name"] == f"{event_type}"]
-
+    df_event = behavior_event[behavior_event["event_name"] == f"{event_type}"]
+    df_event.reset_index(drop=True, inplace=True)
+    # if event_type == "L":
+        
+    
     # time_window = 0.5   # 0.5 seconds before and after each event
     bin_size = 0.025  # 25ms bins
     time_bins = np.arange(-time_window, time_window + bin_size, bin_size)
@@ -42,14 +46,13 @@ def plot_event_ephys(event_type, behavior_event, clusters, time_window):
 
     # Iterate over each event
     for event_idx, event in df_event.iterrows():
+        print(f"Processing event {event_idx + 1} of {event_num}...")
         event_time = event["event_ephys_timestamp"]
         
         # Iterate over each cluster
-        for cluster_idx, cluster in enumerate(clusters):
-            spike_times = cluster["spike_times"]  # Assuming spike_times is the key for spike timestamps
-            
+        for cluster_idx, cluster in enumerate(clusters):            
             # Convert spike times to seconds
-            spike_times = spike_times * 50 / 1e6  
+            spike_times = cluster[0] * 50 / 1e6  
             
             # Count spikes in each time bin
             for bin_idx in range(time_bin_num):
@@ -58,9 +61,10 @@ def plot_event_ephys(event_type, behavior_event, clusters, time_window):
                 spike_count = np.sum((spike_times >= start_time) & (spike_times < end_time))
                 spike_count_array[event_idx, cluster_idx, bin_idx] = spike_count
 
+
     normalized_spike_count_array = np.mean(spike_count_array, axis=0)
     
-    sorted_cluster_indices = pca_on_clusters(normalized_spike_count_array)
+    # sorted_cluster_indices = pca_on_clusters(normalized_spike_count_array)
     
     # Remove the last bin edge to get the bin centers
     time_bin_centers = (time_bins[:-1] + time_bins[1:]) / 2
@@ -82,21 +86,23 @@ def plot_event_ephys(event_type, behavior_event, clusters, time_window):
 
 
 
-event_file = "BehaviroalEvents.parquet"
-cluster_file = "ClustersData.mat"
+event_file = "/mnt/SpatialSequenceLearning/RUN_rYL006/rYL006_P1100/2024-11-21_17-22_rYL006_P1100_LinearTrackStop_25min/session_analytics/BehaviorEvents.parquet"
+cluster_file = "/mnt/SpatialSequenceLearning/RUN_rYL006/rYL006_P1100/2024-11-21_17-22_rYL006_P1100_LinearTrackStop_25min/session_analytics/ephys_829_res.mat"
 
 behavior_event = pd.read_parquet(event_file)
 
 # Convert timestamps to seconds
 behavior_event["event_ephys_timestamp"] = behavior_event["event_ephys_timestamp"] / 1e6
 
-cluster_mat = scipy.io.loadmat("ClustersData.mat")
-clusters = cluster_mat["ClustersData"]
 
+    
+cluster_mat = mat73.loadmat(cluster_file)
+clusters = cluster_mat["spikesByCluster"]
+print("Cluster size:", len(clusters))
 
-lick_raster = plot_event_ephys("L", behavior_event, clusters, time_window=0.5)
+# lick_raster = plot_event_ephys("L", behavior_event, clusters, time_window=0.5)
 
-sound_raster = lick_raster = plot_event_ephys("S", behavior_event, clusters, time_window=0.5)
+sound_raster = plot_event_ephys("S", behavior_event, clusters, time_window=0.5)
 
 reward_raster = plot_event_ephys("R", behavior_event, clusters, time_window=0.5)
 
